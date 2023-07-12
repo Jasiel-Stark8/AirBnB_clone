3db35568786768dda8763ec620d8e4d535f20713
== <==>
2023-07-12 17:13:22 +0000

    connect storage to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
-"""Defines all common attributes/methods for other classes"""
-
 from datetime import datetime
 import uuid
+from models import storage
 
 class BaseModel:
     """Base class for all models"""
@@ -16,6 +15,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -32,3 +32,4 @@ class BaseModel:
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
