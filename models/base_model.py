75b11eaf35fb9005626a0c52ebfea9c75d9df547
== <==>
2023-07-12 11:18:07 +0000

    Project Manual

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ import uuid
 
 class BaseModel():
     """Base class for all models"""
-    
+
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow().isoformat()
