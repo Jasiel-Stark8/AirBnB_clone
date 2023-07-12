2d855c59708bf30510ea444a57ffb48f010793e3
== <==>
2023-07-12 13:49:43 +0000

    add dictionary key/value return

--- models/base_model.py
+++ models/base_model.py
@@ -3,28 +3,32 @@
 from datetime import datetime
 import uuid
 
-
-class BaseModel():
+class BaseModel:
     """Base class for all models"""
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow().isoformat()
-        self.updated_at = datetime.utcnow().isoformat()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """Prints Class, and id as a dictionary"""
+        """Returns a string representation of the instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-    def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime.utcnow().isoformat()
-    
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ of the instance"""
-        return {
-            "__class__": self.__class__.__name__,
-            "id": self.id,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
-        }
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
